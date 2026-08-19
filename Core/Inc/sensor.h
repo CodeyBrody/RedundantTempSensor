d35d5901720fe50e1cf9541587566bec1ba06f09
CodeyBrody <bschippa1@gmail.com>
2026-08-19 16:59:24 -0700

    corrected includes and removed incorrect (for C) 'default value assignment'

--- Core/Inc/sensor.h
+++ Core/Inc/sensor.h
@@ -1,15 +1,15 @@
 #ifndef SENSOR_H
 #define SENSOR_H
-#include <cstdlib>
+#include <stdlib.h>
 #include <math.h>
 
 typedef struct {
     __uint8_t address;
-    float currTemp = NAN;
+    float currTemp;
     char* currTimeStamp[20];
-    float lastTemp = NAN;
+    float lastTemp;
     char* lastTimeStamp[20];
-    __uint8_t faults = 0;
+    __uint8_t faults;
 } Sensor;
 
 #endif

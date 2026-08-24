7ad86c2e2e61464d20ff6a0b4b672b8afbdd35e5
CodeyBrody <bschippa1@gmail.com>
2026-08-24 10:51:24 -0700

    removed sensor functions from this file

--- Core/Inc/functions.h
+++ Core/Inc/functions.h
@@ -3,31 +3,20 @@
 
 #include "sensor.h"
 
-__uint8_t discoverSensorArray(__uint8_t SensorCount, Sensor sensors[]);
-__uint8_t initSensorArray(Sensor sensors[]);
-__uint8_t readTempSensors(Sensor sensors[]);
+extern Sensor sensors[];
+
 float determineTemp(Sensor sensors[]);
 void setAlerts(Sensor sensors[]);
 __uint8_t logData(float displayTemp, Sensor sensors[]);
-void clearFaults(Sensor sensors[]);
 
-__uint8_t discoverSensorArray(__uint8_t SensorCount, Sensor sensors[]){
-    return SensorCount;
-}
 
-__uint8_t initSensorArray(Sensor sensors[]){
-    return 0;
-}
-__uint8_t readTempSensors(Sensor sensors[]){
-    return 0;
-}
 float determineTemp(Sensor sensors[]){
     return 0;
 }
 void setAlerts(Sensor sensors[]){}
+
 __uint8_t logData(float displayTemp, Sensor sensors[]){
     return 0;
 }
-void clearFaults(Sensor sensors[]){}
 
 #endif

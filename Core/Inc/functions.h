3e31b4e5652829a4500049a5d7714ab7771caf95
CodeyBrody <bschippa1@gmail.com>
2026-08-31 15:17:13 -0700

    removed functions that have been implemented elsewhere

--- Core/Inc/functions.h
+++ Core/Inc/functions.h
@@ -5,18 +5,8 @@
 
 extern Sensor sensors[];
 
-float determineTemp(Sensor sensors[]);
 void setAlerts(Sensor sensors[]);
-__uint8_t logData(float displayTemp, Sensor sensors[]);
 
-
-float determineTemp(Sensor sensors[]){
-    return 0;
-}
 void setAlerts(Sensor sensors[]){}
-
-__uint8_t logData(float displayTemp, Sensor sensors[]){
-    return 0;
-}
 
 #endif

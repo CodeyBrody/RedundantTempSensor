db783fe1fd2044593238a2ed967f20f3cc8f0d7b
CodeyBrody <bschippa1@gmail.com>
2026-08-31 14:56:56 -0700

    Implemented 'usb_printf_int', 'usb_println', and 'usb_print)delimiter' functions, (and edited usb_print function NOT to print a newline at the end) in order to fulfill needs of other functions throughout the project.

--- Core/Src/usb_comm.c
+++ Core/Src/usb_comm.c
@@ -1,6 +1,6 @@
 #include "usb_comm.h"
 
-
+extern BUFFER_SIZE;
 extern __uint8_t txBuf[];
 extern UART_HandleTypeDef huart2;
 
@@ -17,10 +17,27 @@ void usb_print(const char *message_body){
     //Send the message body
     strcpy((char*)txBuf, message_body);
     HAL_UART_Transmit(&huart2, txBuf, strlen((char*)txBuf), HAL_MAX_DELAY);
+    return;
+}
+
+void usb_print_delimiter(char delimiter){
+    strcpy((char*)txBuf, delimiter);
+    HAL_UART_Transmit(&huart2, txBuf, strlen((char*)txBuf), HAL_MAX_DELAY);
+}
+
+void usb_println(const char *message_body){
+    //Send the message body
+    strcpy((char*)txBuf, message_body);
+    HAL_UART_Transmit(&huart2, txBuf, strlen((char*)txBuf), HAL_MAX_DELAY);
 
     //Send carriage return and newline
     strcpy((char*)txBuf, "\n\r");
     HAL_UART_Transmit(&huart2, txBuf, strlen((char*)txBuf), HAL_MAX_DELAY);
 
     return;
+}
+
+void usb_printf_int(const char *message, int arg){
+    snprintf((char*)txBuf, BUFFER_SIZE, message, arg);
+    HAL_UART_Transmit(&huart2, txBuf, strlen((char*)txBuf), HAL_MAX_DELAY);
 }

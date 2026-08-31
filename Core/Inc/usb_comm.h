b6e2c564fa9ddd4f2bf05947f607da6921e10883
CodeyBrody <bschippa1@gmail.com>
2026-08-31 15:05:25 -0700

    Added function declarations for new functions in usb_comm.c, and deleted the enum 'messageType' and 'usb_log_message' function which I don't think are going to be used anymore in this project.

--- Core/Inc/usb_comm.h
+++ Core/Inc/usb_comm.h
@@ -9,43 +9,14 @@
 extern __uint8_t txBuf[];
 extern UART_HandleTypeDef huart2;
 
-
 void usb_print(const char *message);
+
+void usb_printf_int(const char *message, int arg);
+
 void print_temp_c(float temp);
 
-enum messageType {
-    E, // Error message type
-    D, // Data message type
-    S, // System data type
-};
+void usb_print_delimiter(char delimiter);
 
-void usb_log_message(enum messageType message_type, const char *message_body){
-    
-    //Print the message header
-    switch (message_type){
-        case E:
-            strcpy((char*)txBuf, "E: ");
-            break;
-        case D:
-            strcpy((char*)txBuf, "D: ");
-            break;
-        case S:
-            strcpy((char*)txBuf, "S: ");
-            break;
-    }
-
-    //Send the message header
-    HAL_UART_Transmit(&huart2, txBuf, strlen((char*)txBuf), HAL_MAX_DELAY);
-
-    //Send the message body
-    strcpy((char*)txBuf, message_body);
-    HAL_UART_Transmit(&huart2, txBuf, strlen((char*)txBuf), HAL_MAX_DELAY);
-
-    //Send carriage return and newline
-    strcpy((char*)txBuf, "\n\r");
-    HAL_UART_Transmit(&huart2, txBuf, strlen((char*)txBuf), HAL_MAX_DELAY);
-
-    return;
-}
+void usb_println(const char *message_body);
 
 #endif /*USB_COM_H*/

d41e86934b0f9223a926501e1706950359871357
CodeyBrody <bschippa1@gmail.com>
2026-09-02 15:31:49 -0700

    moved 'LEDVals' definition out of this file (into alerts.c)

--- Core/Inc/alerts.h
+++ Core/Inc/alerts.h
@@ -7,8 +7,6 @@
 
 extern SPI_HandleTypeDef hspi1;
 
-static __uint16_t LEDVals = 0; //Initializes a variable that is used to control the shift register outputs 
-
 void setAlerts(Sensor sensors[]);
 
 #endif
